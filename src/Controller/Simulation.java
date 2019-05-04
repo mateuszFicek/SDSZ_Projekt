9925564900c812e8c04a45a43079a85ac2b654af
michalkilian <mkilian@mielec.com.pl>
2019-05-04 12:01:37 +0200

    Add annimation timer -> possibilty to render animation in simulation screen

--- src/Controller/Simulation.java
+++ src/Controller/Simulation.java
@@ -1,15 +1,38 @@
 package Controller;
 
+import javafx.animation.AnimationTimer;
 import javafx.event.ActionEvent;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.stage.Stage;
+import javafx.scene.control.Label;
 
-import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class Simulation extends BaseController{
+public class Simulation extends BaseController implements Initializable {
     public Button backButton;
 
+    public Label simLabel;
+    public Button magicStartButton;
+
+    AnimationTimer h = new AnimationTimer() {
+        int i = 0;
+
+        @Override
+        public void handle(long now) {
+            if (i % 20 == 0) {
+                simLabel.setText(String.valueOf(i));
+            }
+            i += 1;
+        }
+    };
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    public void startSimulation(ActionEvent event) {
+    }
 }
