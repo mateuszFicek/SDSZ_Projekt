6df47b332c7dc23d1fd9d28c9d6725815ff6bf1e
michalkilian <mkilian@mielec.com.pl>
2019-05-05 12:32:57 +0200

    Code refactor and velocity calculation improvement

--- src/Model/Vehicles/Vehicle.java
+++ src/Model/Vehicles/Vehicle.java
@@ -1,4 +1,5 @@
 package Model.Vehicles;
+
 import Model.Highway.Cell;
 
 import java.util.Random;
@@ -7,67 +8,62 @@ public class Vehicle {
     protected int velocity;
     public Cell[][] neighbourhood;
     protected int maxVelocity;
-    protected int distanceToNextCarInFront =0;
+    protected int distanceToNextCarInFront = 0;
 
-    public Vehicle(){
+    public Vehicle() {
         neighbourhood = new Cell[3][11];
-        for(int i = 0; i < neighbourhood.length; ++i){
-            for(int j = 0; j < neighbourhood[i].length; ++j){
+        for (int i = 0; i < neighbourhood.length; ++i) {
+            for (int j = 0; j < neighbourhood[i].length; ++j) {
                 neighbourhood[i][j] = new Cell();
             }
         }
 
     }
 
-    public void decreaseVelocity(int velocityChange)
-    {
-        velocity -=velocityChange;
+    public void decreaseVelocity(int velocityChange) {
+        velocity -= velocityChange;
     }
 
-    public void increaseVelocity(int velocityChange)
-    {
-        velocity +=velocityChange;
+    public void increaseVelocity(int velocityChange) {
+        velocity += velocityChange;
     }
 
-    public int getVelocity() {return velocity;}
+    public int getVelocity() {
+        return velocity;
+    }
 
-    public void calculateDistanceToNextFrontVehicle()
-    {
-        for(int i = (neighbourhood[1].length/2)+1, j = 1; i< neighbourhood[1].length; i++)
-        {
+    public void calculateDistanceToNextFrontVehicle() {
+        for (int i = (neighbourhood[1].length / 2) + 1, j = 1; i < neighbourhood[1].length; i++) {
 //            System.out.println(i);
-            if(!neighbourhood[1][i].occupied)
-            {
+            if (!neighbourhood[1][i].occupied) {
                 distanceToNextCarInFront = j;
                 j++;
-            }
-            else{
+            } else {
                 distanceToNextCarInFront = j;
-                break;
+                return;
             }
+
         }
+        distanceToNextCarInFront += 1;
     }
 
-    public void calculateNextVelocity()
-    {
+    public void calculateNextVelocity() {
         double probability = new Random().nextDouble();
         calculateDistanceToNextFrontVehicle();
-        System.out.println("---" + distanceToNextCarInFront + "---");
-        if(distanceToNextCarInFront <= velocity || probability < 0.1){
+        System.out.println("---" + distanceToNextCarInFront + "---" + probability);
+        if (distanceToNextCarInFront <= velocity || (probability < 0.1 && velocity == 5)) {
             SlowDown();
-        } else if(probability >= 0.7){
+        } else if (probability >= 0.7 || velocity <= 3) {
             SpeedUp();
         }
     }
 
-    private void SpeedUp()
-    {
-            velocity = Math.min(velocity +1, maxVelocity);
+    private void SpeedUp() {
+        velocity = Math.min(velocity + 1, maxVelocity);
     }
 
-    private void SlowDown()
-    {
-            velocity = Math.min(velocity, distanceToNextCarInFront -1);
+    private void SlowDown() {
+        velocity = Math.max(Math.min(velocity - 1, distanceToNextCarInFront - 1), 0);
     }
 
 
